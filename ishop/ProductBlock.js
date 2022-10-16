a26f3327fbef373ba7e5f4e0561eb15759acd1d7
koviatsinets <koviatsinets@gmail.com>
2022-10-16 10:14:30 +0300

    add: style table

--- ishop/ProductBlock.js
+++ ishop/ProductBlock.js
@@ -11,19 +11,30 @@ var ProductBlock = React.createClass({
         console.log(this.props.products[a]);
    
         var currentCode=
-          React.DOM.div({key:currentProduct.id,className:'Product'},
-            React.DOM.span({className:'Product'},currentProduct.product),
-            React.DOM.span({className:'Price'},currentProduct.price),
-            React.DOM.span({className:'Amount'},currentProduct.amount),
-            React.DOM.span({className:'URL'},currentProduct.amount),
+          React.DOM.tr({key:currentProduct.id},
+            React.DOM.td({}, currentProduct.product),
+            React.DOM.td({}, currentProduct.price),
+            React.DOM.td({}, currentProduct.amount),
+            React.DOM.td({},
+              React.DOM.img({className:'img', src:`${currentProduct.url}`}),
+            ),
           );
         productsCode.push(currentCode);
         
       }
-      return React.DOM.div( {className:'ProductBlock'},
-        React.DOM.p( {className:'MarketName'}, this.props.title ),
-        React.DOM.div( {className:'Products'}, productsCode ),
-      );
+      return React.DOM.div( {className:''},
+        React.DOM.p( {className:'title'}, this.props.title ),
+        React.DOM.table( {},
+          React.DOM.thead( {}, 
+          React.DOM.tr( {},
+              React.DOM.th( {}, 'Название продукта' ),
+              React.DOM.th( {}, 'Стоимость (руб.)' ),
+              React.DOM.th( {}, 'Количество на складе (шт.)' ),
+              React.DOM.th( {}, 'Изображение' ),
+            ),
+          ),
+          React.DOM.tbody( {}, productsCode ),
+        ),
+        )
     },
-  
   });
