45c11805200222b99ab284607d57157c0a12904b
koviatsinets <koviatsinets@gmail.com>
2022-10-16 10:53:12 +0300

    add: map

--- ishop/ProductBlock.js
+++ ishop/ProductBlock.js
@@ -4,24 +4,6 @@ var ProductBlock = React.createClass({
 
     render: function() {
 
-      var productsCode=[];
-
-      for ( var a=0; a<this.props.products.length; a++ ) {
-        var currentProduct = this.props.products[a];
-        console.log(this.props.products[a]);
-   
-        var currentCode=
-          React.DOM.tr({key:currentProduct.id},
-            React.DOM.td({}, currentProduct.product),
-            React.DOM.td({}, currentProduct.price),
-            React.DOM.td({}, currentProduct.amount),
-            React.DOM.td({},
-              React.DOM.img({className:'img', src:`${currentProduct.url}`}),
-            ),
-          );
-        productsCode.push(currentCode);
-        
-      }
       return React.DOM.div( {className:''},
         React.DOM.p( {className:'title'}, this.props.title ),
         React.DOM.table( {},
@@ -31,10 +13,19 @@ var ProductBlock = React.createClass({
               React.DOM.th( {}, 'Стоимость (руб.)' ),
               React.DOM.th( {}, 'Количество на складе (шт.)' ),
               React.DOM.th( {}, 'Изображение' ),
-            ),
+            )
           ),
-          React.DOM.tbody( {}, productsCode ),
-        ),
+          React.DOM.tbody( {}, this.props.products.map(el => 
+            React.DOM.tr({key:el.id},
+              React.DOM.td({}, el.product),
+              React.DOM.td({}, el.price),
+              React.DOM.td({}, el.amount),
+              React.DOM.td({},
+                React.DOM.img({className:'img', src:`${el.url}`}),
+              )
+            )
+          ))
         )
-    },
+      )
+    }
   });
