aa7597f84d8975d28c0d10868911553594758ef9
koviatsinets <koviatsinets@gmail.com>
2022-11-07 18:52:08 +0300

    refactor: reduce

--- RainbowFrame/src/components/RainbowFrame.js
+++ RainbowFrame/src/components/RainbowFrame.js
@@ -5,11 +5,10 @@ import './RainbowFrame.css';
 class RainbowFrame extends React.Component {
 
   render() {
-    var arr = this.props.colors.slice()
-    arr.unshift(this.props.children)
-    var res = arr.reduce((acc, el) => <div style={{border:`solid 5px ${el}`, padding:"5px"}}>{acc}</div>)
 
-    return <div style={{textAlign:"center"}}>{res}</div>
+    return <div style={{textAlign:"center"}}>
+      {this.props.colors.slice().reduce((acc, el)=><div style={{borderColor: el}}>{acc}</div>,this.props.children)}
+    </div>
   }
 }
 
