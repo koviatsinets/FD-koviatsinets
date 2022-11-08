7cc89d2468cc257cd0f6bf236020daffb7371a2b
koviatsinets <koviatsinets@gmail.com>
2022-11-08 23:15:28 +0300

    add: map

--- br2jsx/src/components/BR2JSX.js
+++ br2jsx/src/components/BR2JSX.js
@@ -4,22 +4,27 @@ import './BR2JSX.css';
 
 class BR2JSX extends React.Component {
 
+  state = {
+    text: this.props.text,
+  };
+
   render() {
+   
     return (
       <div>
         {
-          this.state.text.split(/<br\s*\/?>/g).map((el) => {
+          this.state.text.split(/<br\s*\/?>/g).map((el, i) => {
             return (
-              <>
+              <span key={i}>
                 {el}
                 <br/>
-              </>
+              </span>
             )
           })
         }
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default BR2JSX;
