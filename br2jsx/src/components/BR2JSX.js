e5a9096c2ac14a03cd09190ac30c394c699bd251
koviatsinets <koviatsinets@gmail.com>
2022-11-10 22:21:40 +0300

    refactor: add constructor

--- br2jsx/src/components/BR2JSX.js
+++ br2jsx/src/components/BR2JSX.js
@@ -4,20 +4,28 @@ import './BR2JSX.css';
 
 class BR2JSX extends React.Component {
 
-  render() {
-   
-    var newArr = []
-    var arr = this.props.text.split(/<br\s*\/?>/g);
+  state = {
+    arr: this.props.text
+  }
 
+  constructor(props) {
+    super(props);
+    var newArr = []
+    var arr = this.state.arr.split(/<br\s*\/?>/g);
     for (let i = 0; i <= arr.length - 1; i++) {
       newArr.push(arr[i]);
       if (i !== arr.length - 1) {
       newArr.push(<br key={i}/>)
       }
     }
+    this.state.arr = newArr;
+  }
+
+  render() {
+   
 
     return (
-      <div>{newArr}</div>
+      <div>{this.state.arr}</div>
     );
   };
 };
